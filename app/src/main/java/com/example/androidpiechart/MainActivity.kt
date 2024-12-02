1913b88691528aee6008f1619b0a512d54b11134
JangWoojun <woojun0107@naver.com>
2024-12-02 09:32:54 +0900

    feat: 외부에서 PieChartView 색상 및 비율 조정 가능하게 변경

--- app/src/main/java/com/example/androidpiechart/MainActivity.kt
+++ app/src/main/java/com/example/androidpiechart/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidpiechart
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,5 +17,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val pieChartView = findViewById<PieChartView>(R.id.chart)
+        pieChartView.setData(listOf(30f, 30f, 30f), listOf(Color.RED, Color.GREEN, Color.BLUE))
+
     }
 }
--- app/src/main/java/com/example/androidpiechart/PieChartView.kt
+++ app/src/main/java/com/example/androidpiechart/PieChartView.kt
@@ -7,15 +7,32 @@ import android.view.View
 
 class PieChartView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val colors = listOf(Color.RED, Color.YELLOW, Color.BLUE)
-    private val percentages = listOf(20f, 30f, 50f)
+    private var colors: List<Int> = listOf(Color.RED, Color.YELLOW, Color.BLUE)
+    private var percentages: List<Float> = listOf(20f, 30f, 50f)
+    private val rectF = RectF()
+
+    init {
+        context.theme.obtainStyledAttributes(attrs, R.styleable.PieChartView, 0, 0).apply {
+            try {
+            } finally {
+                recycle()
+            }
+        }
+    }
+
+    fun setData(percentages: List<Float>, colors: List<Int>) {
+        this.percentages = percentages
+        this.colors = colors
+        invalidate()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        rectF.set(0f, 0f, w.toFloat(), h.toFloat())
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        val width = width.toFloat()
-        val height = height.toFloat()
-        val rectF = RectF(0f, 0f, width, height)
-        
         var startAngle = 0f
         for (i in percentages.indices) {
             paint.color = colors[i]
